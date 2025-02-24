60e2c060ff8bdd3747ba0d7d6b6aa7f6a0aa9f12
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-02-24 16:12:07 +0530

    feat: (Expense tracker)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { ExpenseTracker } from "./components/ExpenseTracker";
 import RecipeApp from "./components/RecipeApp";
 
 export default function App() {
   return (
     <>
-     <RecipeApp />
+     {/* <RecipeApp /> */}
+     <ExpenseTracker />
     </>
   )
 }
